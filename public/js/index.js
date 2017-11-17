43b3c90e1ed5e436da102f0ca09e5bc4aeb29fd9
hhchaudh <haaris.h.chaudhry@gmail.com>
2017-11-16 21:49:34 -0600

    Scroll to bottom is user is close to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 
 let socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  let messages = $('#messages');
+  let newMessage = messages.children('li:last-child');  //last list item in the list
+  // Heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -21,10 +37,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
-  // let li = $('<li></li>');
-  // li.text(`${formattedTime} ${message.from}: ${message.text}`);
-  //
-  // $('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -38,6 +51,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
